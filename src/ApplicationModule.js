16030aa8371e0df80efa5e90e46132c10a4645d7
Jason Best <jason.best@swiftpage.com>
2013-10-14 17:04:12 -0700

    Export contact to local contact book on phone. Export is not registered if the phonegap functionality is not present.

--- src/ApplicationModule.js
+++ src/ApplicationModule.js
@@ -39,9 +39,13 @@ define('Mobile/Bootcamp/ApplicationModule', [
             this.registerContactCustomizations();
         },
         registerContactCustomizations: function() {
+            if (!navigator.contacts) {
+                return;
+            }
+
             this.registerCustomization('detail/tools', 'contact_detail', {
                 at: function(tool) {
-                    return tool.id === 'edit'
+                    return tool.id === 'edit';
                 },
                 type: 'insert',
                 where: 'after',
@@ -49,7 +53,92 @@ define('Mobile/Bootcamp/ApplicationModule', [
                     id: 'export-action',
                     icon: 'content/images/icons/Contacts_24x24.png',
                     fn: function() { 
-                        // TODO: Fill in code to export to native contact list here, using phonegap
+                        var view, contact, entry;
+                        view = App.getView('contact_detail');
+                        if (!view || !view.entry) {
+                            console.error('Error retrieving view or the view\'s entry');
+                        }
+
+                        entry = view.entry;
+
+                        // See http://docs.phonegap.com/en/edge/cordova_contacts_contacts.md.html#Contact
+                        contact = navigator.contacts.create({
+                            displayName: entry.$descriptor, // DOMString
+                            name: {
+                                // ContactName
+                                formatted: entry.$descriptor,
+                                familyName: entry.LastName,
+                                givenName: entry.FirstName,
+                                middleName: entry.MiddleName,
+                                honorificPrefix: entry.Prefix,
+                                honorificSuffix: entry.Suffix 
+                            },
+                            nickName: entry.$descriptor, // DOMString
+                            phoneNumbers: [
+                                // ContactField
+                                {
+                                    type: 'home',
+                                    value: entry.HomePhone,
+                                    pref: false
+                                },
+                                {
+                                    type: 'work',
+                                    value: entry.WorkPhone,
+                                    pref: true
+                                },
+                                {
+                                    type: 'fax',
+                                    value: entry.Fax,
+                                    pref: false
+                                }
+                            ],
+                            emails: [
+                                // ContactField
+                                {
+                                    type: 'work',
+                                    value: entry.Email,
+                                    pref: true
+                                }
+                            ],
+                            addresses: [
+                                //ContactAddress
+                                {
+                                    pref: true,
+                                    type: 'work',
+                                    formatted: entry.Address.FullAddress,
+                                    streetAddress: entry.Address.StreetAddress,
+                                    locality: entry.Address.City,
+                                    region: entry.Address.State,
+                                    postalCode: entry.Address.PostalCode,
+                                    country: entry.Address.Country 
+                                }
+                            ],
+                            organizations: [
+                                // ContactOrganization
+                                {
+                                    pref: true,
+                                    type: 'work',
+                                    name: entry.AccountName,
+                                    department: '',
+                                    title: entry.Title 
+                                }
+                            ],
+                            note: 'Imported from Saleslogix Mobile', // DOMString
+                            urls: [
+                                // ContactField
+                                {
+                                    type: 'work',
+                                    value: entry.WebAddress,
+                                    pref: true
+                                }
+                            ]
+                        });
+
+                        contact.save(function(contact) {
+                            alert('Contact saved!');
+                        }, function(contactError) {
+                            alert('Error savig contact!');
+                        });
                     }
                 }
             });
